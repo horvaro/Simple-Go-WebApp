bc273fa41fdba0b51d03ed187a5fd5e687c3a003
Robert Horvath <robert.horvath@noser.com>
2019-05-13 15:57:50 +0200

    Add simple unit test

--- main.go
+++ main.go
@@ -15,12 +15,6 @@ type Welcome struct {
 
 //Go application entrypoint
 func main() {
-	//We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
-	// the relative path). We wrap it in a call to template.Must() which handles any errors
-	// and halts if there are fatal errors
-
-	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
-
 	//Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
 	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
 	//html can refer to when looking for our css and other files.
@@ -34,25 +28,33 @@ func main() {
 	//It is important to note the final url can be whatever we like, so long as we are consistent.
 
 	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		//Instantiate a Welcome struct object and pass in some random information.
-		//We shall get the name of the user as a query parameter from the URL
-		welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
-
-		//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
-		if name := r.FormValue("name"); name != "" {
-			welcome.Name = name
-		}
-		//If errors show an internal server error message
-		//I also pass the welcome struct to the welcome-template.html file.
-		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	http.HandleFunc("/", WelcomeHandler)
 
 	fmt.Println("Listening on PORT 8080")
 	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
 	//Print any errors from starting the webserver using fmt
 	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	//Instantiate a Welcome struct object and pass in some random information.
+	//We shall get the name of the user as a query parameter from the URL
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+
+	//We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
+	// the relative path). We wrap it in a call to template.Must() which handles any errors
+	// and halts if there are fatal errors
+
+	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
+
+	//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
+	if name := r.FormValue("name"); name != "" {
+		welcome.Name = name
+	}
+	//If errors show an internal server error message
+	//I also pass the welcome struct to the welcome-template.html file.
+	if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
